7d27ba8bc6a39577b40ba84a58eb3420c27b76f8
Adrien Quillet <adrienquillet@gmail.com>
2019-02-18 22:26:25 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
+      </Fragment>
     );
   }
 }
---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        Hello from Cart
+        <div>...</div>
+      </Fragment>
+    );
+  }
+}
+
+export default Cart;
